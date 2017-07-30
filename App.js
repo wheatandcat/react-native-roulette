d96d7da9ab1fc82c5fb7bd23f005443ae31afa3a
“wheatandcat” <wheatandcat@gmail.com>
2017-07-31 04:34:33 +0900

    init

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
+// @flow
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Provider } from "react-redux"
+import { Scene, Router } from "react-native-router-flux"
+import Roulette from "./redux/containers/Roulette"
+import createStore from "./redux/createStore"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    )
-  }
-}
+const store = createStore()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-})
+export default () =>
+  <Provider store={store}>
+    <Router>
+      <Scene key="root">
+        <Scene key="pageA" title="PageA" component={Roulette} />
+      </Scene>
+    </Router>
+  </Provider>
--- components/Roulette/Circle.js
+++ components/Roulette/Circle.js
@@ -1,23 +1,85 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// @flow
+import React, { Component } from "react"
+import { StyleSheet, View } from "react-native"
+import { ROULETTE_STATUS_START } from "../../redux/modules/roulette"
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+type State = {
+  count: number
+}
+type Props = {
+  status: number,
+  /* eslint-disable react/no-unused-prop-types */
+  stop: () => void
 }
 
+type DefaultProps = { status: number }
+
 const styles = StyleSheet.create({
-  container: {
+  center: {
+    top: 30,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  yinyangMain: {
+    width: 150,
+    height: 150,
+    borderColor: "red",
+    borderTopWidth: 2,
+    borderLeftWidth: 2,
+    borderBottomWidth: 75,
+    borderRightWidth: 2,
+    borderRadius: 75
   }
-});
+})
+
+let self: {
+  props: Props
+}
+let up = 50
+let count = 0
+
+const tick = () => {
+  if (up <= 0) {
+    self.props.stop()
+    return
+  }
+
+  if (self.props.status !== ROULETTE_STATUS_START) return
+
+  if (count % 2 === 0) {
+    up -= 1
+  }
+
+  self.setState({
+    count: self.state.count + up
+  })
+
+  count += 1
+}
+
+export default class Circle extends Component<DefaultProps, Props, State> {
+  static defaultProps = { status: 0 }
+  state = { count: 0 }
+  componentDidMount() {
+    self = this
+    this.interval = setInterval(tick, 1)
+  }
+  componentWillReceiveProps() {
+    if (this.props.status === ROULETTE_STATUS_START) return
+    up = 50
+    count = 0
+  }
+  props: Props
+
+  render() {
+    return (
+      <View style={styles.center}>
+        <View style={{ transform: [{ rotate: `${this.state.count}deg` }] }}>
+          <View style={styles.yinyangMain} />
+        </View>
+      </View>
+    )
+  }
+}
